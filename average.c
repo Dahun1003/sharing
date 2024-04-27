08586f083f3fbbd328a1d8773eb4992a1e7a9c17
Dahun1003 <165646332+Dahun1003@users.noreply.github.com>
2024-04-27 22:55:12 +0900

    Create average.c

---
+++ average.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+	int num1, i, j;
+	scanf("%d", &num1);
+	
+	double arr[1000];
+
+	double max = 0, total = 0;
+
+	for (i = 0; i < num1; i++) {
+		scanf("%lf", &arr[i]);
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+	}
+	for (j = 0; j < num1; j++) {
+		arr[j] = (arr[j] / max) * 100;
+		total += arr[j];
+	}
+
+	printf("%lf", total/num1);
+	
+  return 0;
+}
